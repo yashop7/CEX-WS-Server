ff94883f3d85dd80a3c7a9730c623b9e12d18847
Yash <139779840+yashop7@users.noreply.github.com>
2026-04-22 04:56:33 +0530

    Refactor code structure for improved readability and maintainability

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
 import { config } from "dotenv";
 config();
-export const pusherCluster = process.env.PUSHER_CLUSTER || "";
-export const pusherId = process.env.PUSHER_APP_ID || "";
+export const redisUrl = process.env.REDIS_ENGINE_DOWNSTREAM_URL || "";
 export const port = process.env.PORT || 3001;
